5250b8e0c048b858ef370b624de49171ccc26066
Piyushnmj <piyushnmj@yahoo.com>
2022-11-21 20:07:33 +0530

    Welcome

---
+++ RegExPattern/RegExPattern/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RegExPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expressions");
+        }
+    }
+}
